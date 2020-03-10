1563790e6c55f42cfdb02b0d537d1ad70f1c376c
DevilInside <gpaciulli100@gmail.com>
2020-03-10 11:09:34 -0300

    tests configuration

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :picture
 
+  validates_presence_of :name, :description, :price, :picture
+  validates :price, numericality: { greater_than: 0 }
+
 end
