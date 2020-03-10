28721826d52b42661c90861de88387de9bfce8bf
Guilherme Paciulli <guilherme.paciulli@idwall.com>
2020-03-10 17:56:06 -0300

    more testing

--- spec/requests/product_requests_spec.rb
+++ spec/requests/product_requests_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Products API", type: :request do
 
   let!(:products) { create_list(:product, 10) }
+  let!(:first_product) { products.first.id }
 
   describe "get /products" do
     context 'when no params are informed' do
@@ -17,16 +18,97 @@ RSpec.describe "Products API", type: :request do
         expect(response).to have_http_status(200)
       end
     end
-    context 'when params are informed' do
-      before { get '/products/'+products.first.id.to_s }
+  end
 
-      it 'should return the product with id 1' do
-        expect(json["data"]).not_to be_empty
+  describe "get /products/:id" do
+    before { get '/products/'+first_product.to_s }
+
+    context 'when the record exists' do
+      it 'should return the product data' do
+        expect(json).not_to be_empty
+        expect(json["data"]['id']).to eq(first_product.to_s)
       end
 
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
+    end
+
+    context 'when the record does not exists' do
+      let(:first_product) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Product with 'id'=100/)
+      end
+    end
+  end
+
+  describe 'POST /products' do
+    let(:valid_attributes) {
+                              {
+                               name: 'Jetski Fera',
+                               price: 10.5,
+                               description: 'Jetski muito fera',
+                               picture: Rack::Test::UploadedFile.new('spec/support/fixtures/img.jpg', 'image/jpg')
+                              }.to_json
+                            }
+    let(:invalid_attributes) {
+                              {
+                               name: 'Jetski Fera',
+                               price: 10.5,
+                               picture: Rack::Test::UploadedFile.new('spec/support/fixtures/img.jpg', 'image/jpg')
+                              }.to_json
+                            }
+
+    context 'when the request is valid' do
+      before { post '/products', params: valid_attributes }
+      it 'creates a fast product ad' do
+        expect(json['data']['name']).to eq('Jetski Fera')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/products', params: invalid_attributes }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: description can't be blank/)
+      end
+    end
+  end
+
+  describe 'PUT /products/:id' do
+    let(:valid_attributes) { { name: 'Jetski 2.0' }.to_json }
+
+    context 'when the record exists' do
+      before { put "/products/#{first_product}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  describe 'DELETE /products/:id' do
+    before { delete "/products/#{first_product}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
     end
   end
 
