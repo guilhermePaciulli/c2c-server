117f78f882ac41a898ee8a5c5bfa6acf90217a46
Guilherme Paciulli <guilherme.paciulli@idwall.com>
2020-03-10 15:53:21 -0300

    first request tests

---
+++ spec/requests/product_requests_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Products API", type: :request do
+
+  let!(:products) { create_list(:product, 10) }
+
+  describe "get /products" do
+    context 'when no params are informed' do
+      before { get "/products" }
+
+      it 'should return all products' do
+        expect(json).not_to be_empty
+        expect(json["data"].size).to eq(10)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+    context 'when params are informed' do
+      before { get '/products/'+products.first.id.to_s }
+
+      it 'should return the product with id 1' do
+        expect(json["data"]).not_to be_empty
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+end
