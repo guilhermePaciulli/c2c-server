5ae4432e4a0f88e5b72563ea72906bd9639ac17a
Guilherme Paciulli <gpaciulli100@gmail.com>
2020-04-05 12:10:57 -0300

    User profile picture  (#7)
    * tests configuration
    
    * first request tests
    
    * more testing
    
    * final tests
    
    * ci yml
    
    * configuring CI
    
    * Create main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * update main.yml
    
    * Update CI
    
    * update gemfile lock
    
    * Authentication (#4)
    
    * feature(auth): creating authorization controller
    
    * feat(authentication): create users, create jwt and tests for user creation
    
    * tests(login): users facotry and login specs
    
    * feat(authentication): to create an ad you have to be logged in
    
    * tests(auth): fix tests for product creation
    
    * tests(auth): add more authentication tests
    
    * Add profile picture to user and indexing single user (#6)
    
    * apply gitignore
    
    * updating CI
    
    * updating CI
    
    Co-authored-by: Guilherme Paciulli <guilherme.paciulli@idwall.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,28 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:show]
+  before_action :set_user, only: [:show]
 
-  # POST /users
   def create
     @user = User.new(user_params)
     @user.save!
     render :show, status: :created
   end
 
-  private
+  def show
+    if @user
+      render json: UserSerializer.new(@user).serializable_hash
+    else
+      render json: OpenUserSerializer.new(current_user).serializable_hash
+    end
+  end
 
-    # Only allow a trusted parameter "white list" through.
+  private
+    def set_user
+      if params[:id]
+        @user = User.find(params[:id])
+      end
+    end
     def user_params
-      params.permit(:email, :password, :password_confirmation, :surname, :cpf, :name)
+      params.permit(:email, :password, :password_confirmation, :surname, :cpf, :name, :profile_picture)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_one_attached :profile_picture
+
   has_secure_password
-  validates_presence_of :email, :name, :surname, :cpf, :password_digest
+  validates_presence_of :email, :name, :surname, :cpf, :password_digest, :profile_picture
   validates_uniqueness_of :email, :cpf
+  validates :password, length: { in: 8..20 }
 end
