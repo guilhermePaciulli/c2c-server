bc84565ad45f15622dbb1697137934ef7a78ff2e
Guilherme Paciulli <gpaciulli100@gmail.com>
2020-04-05 12:10:57 -0300

    User profile picture  (#7)
    * tests configuration
    
    * first request tests
    
    * more testing
    
    * final tests
    
    * ci yml
    
    * configuring CI
    
    * Create main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * update main.yml
    
    * Update CI
    
    * update gemfile lock
    
    * Authentication (#4)
    
    * feature(auth): creating authorization controller
    
    * feat(authentication): create users, create jwt and tests for user creation
    
    * tests(login): users facotry and login specs
    
    * feat(authentication): to create an ad you have to be logged in
    
    * tests(auth): fix tests for product creation
    
    * tests(auth): add more authentication tests
    
    * Add profile picture to user and indexing single user (#6)
    
    * apply gitignore
    
    * updating CI
    
    * updating CI
    
    Co-authored-by: Guilherme Paciulli <guilherme.paciulli@idwall.com>

--- spec/requests/users_requests_spec.rb
+++ spec/requests/users_requests_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Users creation", type: :request do
 
+  let!(:user_id) { create(:user).id.to_s }
+
   describe "POST #create" do
     context "with valid params" do
       before { post '/users', params: { :name => "Rogerinho",
                                         :surname => "do Ingá",
                                         :cpf => "41548807800",
                                         :password => "password@1",
-                                        :email => "roger.inga@gmail.com" } }
+                                        :email => "roger.inga@gmail.com",
+                                        :profile_picture => Rack::Test::UploadedFile.new('spec/support/fixtures/img.jpg', 'image/jpg') } }
 
       it "creates a new User" do
         expect(response).to have_http_status(:created)
@@ -29,6 +32,34 @@ RSpec.describe "Users creation", type: :request do
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
+  end
+
+  describe "GET #show" do
+
+    context "when user is authenticated with no params" do
+      before { get '/user_info', headers: authenticated_header }
+
+      it "should return the user" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "render a JSON response with the user attributes" do
+        expect(json['data']['attributes']['name']).not_to be_nil
+      end
+    end
+
+    context "when user is authenticated with valid params" do
+      before { get '/user_info/'+user_id, headers: authenticated_header }
+
+      it "should return the user" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "render a JSON response with the user attributes" do
+        expect(json['data']['attributes']['name']).not_to be_nil
+      end
+    end
+
   end
 
 end
