50da50438c46e40fce7c8fea27c4c6c39e3ba4a1
Guilherme Paciulli <gpaciulli100@gmail.com>
2020-04-05 11:12:37 -0300

    Add profile picture to user and indexing single user (#6)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   post 'users' => 'users#create'
   post 'user_token' => 'user_token#create'
+  get 'user_info/:id' => 'users#show'
+  get 'user_info' => 'users#show'
   resources :products
 
 end
