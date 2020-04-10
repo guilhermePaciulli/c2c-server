fecd07b5d13be2876b2525216536415091931a7a
Guilherme Paciulli <gpaciulli100@gmail.com>
2020-04-10 19:30:56 -0300

    Creating association for Product Owner

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -10,4 +10,11 @@ module RequestSpecHelper
     }
   end
 
+  def authenticated_header_for_user_id(user_id)
+    token = Knock::AuthToken.new(payload: { sub: user_id }).token
+    {
+      'Authorization': "Bearer #{token}"
+    }
+  end
+
 end
