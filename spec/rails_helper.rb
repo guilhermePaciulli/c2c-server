2f8267530cc19f063d4d0a44233c100bb3eb9fbe
DevilInside <gpaciulli100@gmail.com>
2020-04-15 23:50:52 -0300

    testing code cov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,8 +4,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 SimpleCov.start
 
-
-
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
