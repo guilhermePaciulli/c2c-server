90318891b35d63c1205423d4f7d28c87b935f6ca
DevilInside <gpaciulli100@gmail.com>
2020-05-20 12:28:32 -0300

    general fixes

--- app/serializers/credit_card_serializer.rb
+++ app/serializers/credit_card_serializer.rb
@@ -3,4 +3,13 @@ class CreditCardSerializer
 
   attributes :number, :owner, :cvv, :expiration
 
+  attribute :number do |object|
+    number = object.number
+    last_digits = number.to_s.length <= 4 ? number : number.to_s.slice(-4..-1)
+    object.number = "**** **** **** #{last_digits}"
+  end
+
+  attribute :cvv do |object|
+    "***"
+  end
 end
